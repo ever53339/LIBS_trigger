90ef7c70754945412a316372f1c3e2d797ac682d
XCDell <caoxuan8872@gmail.com>
2024-11-19 12:38:44 -0800

    adjust the implementation of the server using oop

--- client.py
+++
@@ -1,9 +0,0 @@
-import socketio
-
-client = socketio.Client()
-
-client.connect('http://localhost:1234')
-
-client.emit('pull_trigger', 'measure_button.png')
-
-client.wait()
---
+++ foo.py
@@ -0,0 +1,23 @@
+from pyvda import AppView, get_apps_by_z_order, VirtualDesktop, get_virtual_desktops
+
+
+print(get_virtual_desktops())
+# a = get_apps_by_z_order()
+# a = [k.app_id for k in a]
+# print(a)
+# number_of_active_desktops = len(get_virtual_desktops())
+# print(f"There are {number_of_active_desktops} active desktops")
+
+# current_desktop = VirtualDesktop.current()
+# print(f"Current desktop is number {current_desktop.number}")
+
+# current_window = AppView.current()
+# target_desktop = VirtualDesktop(1)
+# current_window.move(target_desktop)
+# print(f"Moved window {current_window.hwnd} to {target_desktop.number}")
+
+# print("Going to desktop number 1")
+# VirtualDesktop(1).go()
+
+# print("Pinning the current window")
+# AppView.current().pin()
